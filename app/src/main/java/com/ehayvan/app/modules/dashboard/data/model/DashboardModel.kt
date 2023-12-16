ecfa8eb1864f44c274cfa439f0c23caca79ae865
Beyza Aydeniz <beyzaaydeniiz@gmail.com>
2023-12-16 04:06:46 +0300

    #EHA-8# Made UI enhancements, deleted unnecessary variables from model files, and fixed typo errors.

--- app/src/main/java/com/ehayvan/app/modules/dashboard/data/model/DashboardModel.kt
+++ app/src/main/java/com/ehayvan/app/modules/dashboard/data/model/DashboardModel.kt
@@ -5,24 +5,7 @@ import com.ehayvan.app.appcomponents.di.MyApp
 import kotlin.String
 
 data class DashboardModel(
-  /**
-   * TODO Replace with dynamic value
-   */
-  var txtHello: String? = MyApp.getInstance().resources.getString(R.string.lbl_hello)
-  ,
-  /**
-   * TODO Replace with dynamic value
-   */
-  var txtEsther: String? = MyApp.getInstance().resources.getString(R.string.lbl_esther)
-  ,
-  /**
-   * TODO Replace with dynamic value
-   */
-  var txtTitle: String? = MyApp.getInstance().resources.getString(R.string.lbl_uh_oh)
-  ,
-  /**
-   * TODO Replace with dynamic value
-   */
-  var txtBody: String? = MyApp.getInstance().resources.getString(R.string.msg_looks_like_you)
 
+
+  var txtName: String? = MyApp.getInstance().resources.getString(R.string.lbl_esther)
 )
--- app/src/main/java/com/ehayvan/app/modules/editpetrecordstwoempty/data/model/EditPetRecordsTwoEmptyModel.kt
+++ app/src/main/java/com/ehayvan/app/modules/editpetrecordstwoempty/data/model/EditPetRecordsTwoEmptyModel.kt
@@ -5,29 +5,10 @@ import com.ehayvan.app.appcomponents.di.MyApp
 import kotlin.String
 
 data class EditPetRecordsTwoEmptyModel(
-  /**
-   * TODO Replace with dynamic value
-   */
-  var txtBody: String? = MyApp.getInstance().resources.getString(R.string.msg_edit_pet_record)
-  ,
-  /**
-   * TODO Replace with dynamic value
-   */
-  var txtTitle: String? = MyApp.getInstance().resources.getString(R.string.lbl_drops)
-  ,
-  /**
-   * TODO Replace with dynamic value
-   */
+
   var txtTitleOne: String? = MyApp.getInstance().resources.getString(R.string.lbl_no_drops_added)
   ,
-  /**
-   * TODO Replace with dynamic value
-   */
   var txtPetName: String? = MyApp.getInstance().resources.getString(R.string.lbl_dudu)
-  ,
-  /**
-   * TODO Replace with dynamic value
-   */
-  var txtButton: String? = MyApp.getInstance().resources.getString(R.string.lbl_add_drops)
+
 
 )
