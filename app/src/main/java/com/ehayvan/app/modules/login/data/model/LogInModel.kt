5966d27f1bcadf876f51bec47ad5923691b3b833
ituitis20-kilicb20 <kilicb20@itu.edu.tr>
2023-12-23 03:44:21 +0300

    #EHA-7# Log in and register screen are edited. Added EditTexts and their corresponding variables into models.

--- app/src/main/java/com/ehayvan/app/modules/login/data/model/LogInModel.kt
+++ app/src/main/java/com/ehayvan/app/modules/login/data/model/LogInModel.kt
@@ -5,24 +5,7 @@ import com.ehayvan.app.appcomponents.di.MyApp
 import kotlin.String
 
 data class LogInModel(
-  /**
-   * TODO Replace with dynamic value
-   */
-  var txtTitle: String? = MyApp.getInstance().resources.getString(R.string.lbl_log_in)
-  ,
-  /**
-   * TODO Replace with dynamic value
-   */
-  var txtLabel: String? = MyApp.getInstance().resources.getString(R.string.lbl_your_email)
-  ,
-  /**
-   * TODO Replace with dynamic value
-   */
-  var txtLabelOne: String? = MyApp.getInstance().resources.getString(R.string.lbl_password)
-  ,
-  /**
-   * TODO Replace with dynamic value
-   */
-  var txtButton: String? = MyApp.getInstance().resources.getString(R.string.lbl_log_in)
+  var etInputContentOneValue: String? = null,
 
+  var etInputContentTwoValue: String? = null,
 )
--- app/src/main/java/com/ehayvan/app/modules/register/data/model/RegisterModel.kt
+++ app/src/main/java/com/ehayvan/app/modules/register/data/model/RegisterModel.kt
@@ -5,41 +5,9 @@ import com.ehayvan.app.appcomponents.di.MyApp
 import kotlin.String
 
 data class RegisterModel(
-  /**
-   * TODO Replace with dynamic value
-   */
-  var txtTitle: String? = MyApp.getInstance().resources.getString(R.string.lbl_sign_up)
-  ,
-  /**
-   * TODO Replace with dynamic value
-   */
-  var txtBody: String? = MyApp.getInstance().resources.getString(R.string.msg_welcome_please)
-  ,
-  /**
-   * TODO Replace with dynamic value
-   */
-  var txtNameandSurnam: String? =
-      MyApp.getInstance().resources.getString(R.string.msg_name_and_surnam)
-  ,
-  /**
-   * TODO Replace with dynamic value
-   */
-  var txtLabel: String? = MyApp.getInstance().resources.getString(R.string.lbl_your_email)
-  ,
-  /**
-   * TODO Replace with dynamic value
-   */
-  var txtLabelOne: String? = MyApp.getInstance().resources.getString(R.string.lbl_password)
-  ,
-  /**
-   * TODO Replace with dynamic value
-   */
-  var txtButton: String? = MyApp.getInstance().resources.getString(R.string.lbl_sign_up)
-  ,
-  /**
-   * TODO Replace with dynamic value
-   */
-  var txtConfirmation: String? =
-      MyApp.getInstance().resources.getString(R.string.msg_already_have_an)
+  var etInputContentOneValue: String? = null,
 
+  var etInputContentTwoValue: String? = null,
+
+  var etInputContentThreeValue: String? = null
 )
